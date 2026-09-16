26f51eaef56467804feee98e254d991651ac2c06
Jimmy-Trace <james@tracescientific.com>
2026-09-16 11:29:47 -0700

    UI: add platform administration route

---
+++ src/app/platform/page.tsx
@@ -0,0 +1,7 @@
+import { PlatformAdministrationShell } from "@/components/platform-administration-shell";
+
+export const dynamic = "force-dynamic";
+
+export default function PlatformAdministrationPage() {
+  return <PlatformAdministrationShell />;
+}
