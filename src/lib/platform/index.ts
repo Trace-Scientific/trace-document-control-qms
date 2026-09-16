186ccb30bf1bbac3b58ffd430e03061fb0292f19
Jimmy-Trace <james@tracescientific.com>
2026-09-16 11:13:59 -0700

    Platform: export controlled support access

--- src/lib/platform/index.ts
+++ src/lib/platform/index.ts
@@ -2,3 +2,5 @@ export * from "./authorization";
 export * from "./authenticated-request";
 export * from "./permissions";
 export * from "./customer-accounts";
+export * from "./support-access";
+export * from "./support-authenticated-request";
